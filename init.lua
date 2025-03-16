4ab0e52c7eff67ff792745a95b87c7043a2ab483
dreydengys <busterkeel59@gmail.com>
2025-03-16 13:46:19 +0100

    feat: Use `powershell.exe` instead of `cmd.exe` on Windows

--- init.lua
+++ init.lua
@@ -11,4 +11,11 @@ vim.opt.cursorline = true
 -- display line numbers
 vim.o.number = true
 
+if jit.os == "Windows" then
+    vim.opt.shell = "powershell.exe"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+end
+
 vim.print("Config successfuly loaded")
